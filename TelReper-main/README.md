ba5e35bef8bb85dd6411289f6e9bb2330854f0ea
tmq247 <60788746+tmq247@users.noreply.github.com>
2023-12-06 00:05:11 +0700

    Add files via upload

---
+++ TelReper-main/README.md
@@ -0,0 +1,39 @@
+# TelReper
+A tool for reporting telegram channel automatically
+<h1>Usage</h1>
+<ul>
+  <li>
+    <h3>Termux (Android):</h3>
+    <ul>
+    <li><code>apt update</code></li>
+    <li><code>apt upgrade</code></li>
+    <li><code>pkg install python3 python3-pip git -y</code></li>
+    <li><code>git clone https://github.com/e811-py/TelReper</code></li>
+    <li><code>cd TelReper</code></li>
+    <li><code>pip3 install -r requeirments.txt</code></li>
+    <li><code>python3 reper.py</code></li>
+    </ul>
+  </li>
+  <li>
+    <h3>Linux:</h3>
+    <ul>
+    <li><code>sudo apt update</code></li>
+    <li><code>sudo apt upgrade</code></li>
+    <li><code>sudo apt install python3 python3-pip git -y</code></li>
+    <li><code>git clone https://github.com/e811-py/TelReper</code></li>
+    <li><code>cd TelReper</code></li>
+    <li><code>sudo pip3 install -r requeirments.txt</code></li>
+    <li><code>python3 reper.py</code></li>
+    </ul>
+  </li>
+  <li>
+    <h3>Windows</h3>
+    <ul>
+    <li>Install python3 from python.org</li>
+    <li>Download source code from https://github.com/e811-py/TelReper</li>
+    <li>change directory to source code directory</li>
+    <li><code>pip install -r requierments.txt</code></li>
+    <li><code>python reper.py</code></li>
+    </ul>
+  </li>
+</ul>
---
+++ TelReper-main/reper.py
@@ -0,0 +1,134 @@
+# t.me/Mr3rf1  <3
+# t.me/MrSMSBomber
+# Eski khasti beri manba bezan :|
+from telethon.sync import TelegramClient
+from telethon.tl.functions.channels import JoinChannelRequest
+from telethon.errors.rpcerrorlist import PhoneNumberInvalidError
+from telethon import functions, types
+from os import listdir, mkdir
+from sys import argv
+from re import search
+from colorama import Fore
+import asyncio, argparse
+parser = argparse.ArgumentParser(description='Công cụ báo cáo các kênh telegram của t.me/coihaycoc', add_help=False)
+parser.add_argument('-an', '--add-number', help='Thêm tài khoản mới')
+parser.add_argument('-r', '--run', help='Để đếm và chạy', type=int)
+parser.add_argument('-t', '--target', help='Nhập mục tiêu', type=str)
+parser.add_argument('-m', '--mode', help='đặt lý do báo cáo', choices=['spam', 'fake_account', 'violence', 'child_abuse', 'pornography', 'geoirrelevant'])
+parser.add_argument('-re', '--reasons', help='hiển thị danh sách lý do', action='store_true')
+parser.add_argument('-h', '--help', action='store_true')
+args = parser.parse_args()
+try:
+    mkdir('sessions')
+except: pass
+sesis = listdir('sessions')
+sesis.sort()
+api_id = 27062094
+api_hash = '2edbe92cfa9db035248bfc8957ba1b95'
+if args.help:
+    print(f'''Help:
+  -an {Fore.LIGHTBLUE_EX}NUMBER{Fore.RESET}, --add-number {Fore.LIGHTBLUE_EX}NUMBER{Fore.RESET} ~> {Fore.YELLOW}thêm tài khoản vào tập lệnh{Fore.RESET}
+  example: python3 {argv[0]} -an {Fore.LIGHTBLUE_EX}+1512****{Fore.RESET}
+  
+  -r {Fore.LIGHTBLUE_EX}COUNT{Fore.RESET}, --run {Fore.LIGHTBLUE_EX}COUNT{Fore.RESET} ~> {Fore.YELLOW}đặt số lượng báo cáo{Fore.RESET}
+  -t {Fore.LIGHTBLUE_EX}TARGET{Fore.RESET}, --target {Fore.LIGHTBLUE_EX}TARGET{Fore.RESET} ~> {Fore.YELLOW}đặt mục tiêu (không có @){Fore.RESET}
+  -m {Fore.LIGHTBLUE_EX}MODE{Fore.RESET}, --mode {Fore.LIGHTBLUE_EX}MODE{Fore.RESET} ~> {Fore.YELLOW}đặt loại báo cáo (thư rác, ...){Fore.RESET}
+  example: python3 {argv[0]} -r {Fore.LIGHTBLUE_EX}1000{Fore.RESET} -t {Fore.LIGHTBLUE_EX}mmdChannel{Fore.RESET} -m {Fore.LIGHTBLUE_EX}spam{Fore.RESET}
+  
+  -re, --reasons ~> {Fore.YELLOW}show list of reasons for reporting{Fore.RESET}
+  -h, --help ~> {Fore.YELLOW}show help{Fore.RESET}''')
+elif args.reasons:
+    print(f'''List of reasons:
+    {Fore.YELLOW}*{Fore.RESET} spam
+    {Fore.YELLOW}*{Fore.RESET} fake_account
+    {Fore.YELLOW}*{Fore.RESET} violence
+    {Fore.YELLOW}*{Fore.RESET} child_abuse
+    {Fore.YELLOW}*{Fore.RESET} pornography
+    {Fore.YELLOW}*{Fore.RESET} geoirrelevant''')
+elif args.add_number != None:
+    number = args.add_number
+    if sesis != []:
+        nums = [int(search('Ac(\d+)\.session', x).group(1)) for x in sesis]
+        nums.sort()
+        nOfLastAc = int(search('Ac(\d+)\.session', sesis[-1]).group(1))
+        ses = TelegramClient(f'sessions/Ac{nums[-1]+1}', api_id, api_hash)
+        try:
+            ses.start(number)
+            print(f' [{Fore.GREEN}✅{Fore.RESET}] Tài khoản của bạn đã được thêm thành công :D')
+            exit(0)
+        except PhoneNumberInvalidError:
+            print(f' [{Fore.RED}!{Fore.RESET}] Số điện thoại không hợp lệ!{Fore.RESET}')
+    else:
+        ses = TelegramClient(f'sessions/Ac1', api_id, api_hash)
+        try:
+            ses.start(number)
+            print(f' [{Fore.GREEN}✅{Fore.RESET}] Tài khoản của bạn đã được thêm thành công :D')
+            exit(0)
+        except PhoneNumberInvalidError:
+            print(f' [{Fore.RED}!{Fore.RESET}] Số điện thoại không hợp lệ!{Fore.RESET}')
+elif args.add_number == None and args.run != None and args.target != None and args.mode != None:
+    if sesis == []:
+        print(f' [{Fore.RED}!{Fore.RESET}] Vui lòng {Fore.RED}thêm một tài khoản{Fore.RESET} để báo cáo!')
+        exit(0)
+    else:
+        count = int(args.run)
+        target = args.target
+        async def report(client):
+            async with client as cli:
+                selfName = await cli.get_entity('self')
+                selfName = selfName.first_name
+                try: repMes = await cli.get_messages(target, limit=3)
+                except ValueError: print(f' [{Fore.RED}!{Fore.RESET}] Liên kết kênh không hợp lệ!'); exit(0)
+                repMess = []
+                for m in repMes:
+                    repMess.append(m.id)
+                async for dialog in cli.iter_dialogs():
+                    if dialog.is_channel:
+                        if dialog.entity.username == target: exi = True; break
+                    else:
+                        exi = False
+                if not exi:
+                    await cli(JoinChannelRequest(target))
+                    await asyncio.sleep(1)
+                for r in range(count):
+                    # result = await cli(functions.messages.ReportSpamRequest(peer=target))
+                        # functions.account.ReportPeerRequest(peer=target, reason=types.InputReportReasonPornography(), message='This channel sends offensive content'))
+                    if args.mode == 'spam':
+                        result = await cli(functions.messages.ReportRequest(peer=target, id=repMess, reason=types.InputReportReasonSpam(), message="This channel sends offensive content"))                        # functions.account.ReportPeerRequest(peer=target, reason=types.InputReportReasonViolence()))
+                    elif args.mode == 'fake_account':
+                        result = await cli(functions.messages.ReportRequest(peer=target, id=repMess, reason=types.InputReportReasonFake(), message="This channel sends offensive content"))
+                    elif args.mode == 'violence':
+                        result = await cli(functions.messages.ReportRequest(peer=target, id=repMess, reason=types.InputReportReasonViolence(), message="This channel sends offensive content"))
+                    elif args.mode == 'child_abuse':
+                        result = await cli(functions.messages.ReportRequest(peer=target, id=repMess, reason=types.InputReportReasonChildAbuse(), message="This channel sends offensive content"))
+                    elif args.mode == 'pornography':
+                        result = await cli(functions.messages.ReportRequest(peer=target, id=repMess, reason=types.InputReportReasonPornography(), message="This channel sends offensive content"))
+                  
+                    elif args.mode == 'geoirrelevant':
+                        result = await cli(functions.messages.ReportRequest(peer=target, id=repMess, reason=types.InputReportReasonGeoIrrelevant(), message="This channel sends offensive content"))                        # functions.account.ReportPeerRequest(peer=target, reason=types.InputReportReasonViolence()))
+                    if result:
+                        print(f" [{Fore.GREEN}✅{Fore.RESET}] Reported :) Ac:{Fore.YELLOW}{selfName}{Fore.RESET} count:{Fore.LIGHTBLUE_EX}{r}{Fore.RESET}")
+                    else:
+                        print(f" [{Fore.RED}!{Fore.RESET}] Error :( Ac:{Fore.YELLOW}{selfName}{Fore.RESET}, count:{Fore.LIGHTBLUE_EX}{r}{Fore.RESET}")
+        async def main():
+            runLis = []
+            for num in range(1, len(sesis) + 1):
+                exec(
+                    f"runLis.append(report(TelegramClient(f'sessions/Ac{num}', api_id, api_hash)))")
+            await asyncio.gather(*runLis)
+        asyncio.run(main())
+elif args.add_number == None and args.run != None and (args.target == None or args.mode == None):
+    print(f" [{Fore.RED}!{Fore.RESET}] Vui lòng sử dụng định dạng này{Fore.RED}~>{Fore.RESET} python3 {argv[0]} -r 10000 -t mmdChannel -m reportReseaon")
+elif args.add_number == None and args.run == None and args.target == None and args.mode == None:
+    print(f"""
+    _____    _ __    #t.me/{Fore.MAGENTA}coihaycoc{Fore.RESET}    💀
+   |_   _|__| |  _ \ ___ _ __   ___ _ __
+     | |/ _ \ | |_) / _ \ '_ \ / _ \ '__|
+     | |  __/ |  _ <  __/ |_) |  __/ |
+     |_|\___|_|_| \_\___| .__/ \___|_|
+                         |_|
+    
+{Fore.YELLOW}-----------------------------------------------{Fore.RESET}
+ một công cụ báo cáo các kênh telegram của @coihaycoc
+ use --help to see help: python3 {argv[0]} --help
+    """)
